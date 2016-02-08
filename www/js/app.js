ee5cececdf01e4d4414c8c1dd71c7c06f89c532d
dami <droig@live.com>
2016-02-08 19:50:34 -0300

    agrego top ten en blog

--- www/js/app.js
+++ www/js/app.js
@@ -3,132 +3,133 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('kipling', ['ionic', 'kipling.controllers', 'ionic.utils'])
+angular.module('kipling', ['ionic', 'kipling.controllers', 'kipling.services', 'ionic.utils'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(
+                true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/login');
 
-  $stateProvider
+    $stateProvider
 
-  .state('register', {
-    url: '/register',
-    templateUrl: 'templates/register.html',
-    controller: 'RegisterCtrl'
-  })
+        .state('register', {
+        url: '/register',
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterCtrl'
+    })
 
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'LoginCtrl'
-  })
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    })
 
-  .state('loggedin', {
-    url: '/loggedin',
-    templateUrl: 'templates/loggedin.html'
-  })
+    .state('loggedin', {
+        url: '/loggedin',
+        templateUrl: 'templates/loggedin.html'
+    })
 
-  .state('loggedin.perfil', {
-    url: '/perfil',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/perfil.html',
-        controller: 'PerfilCtrl'
-      }
-    }
-  })
+    .state('loggedin.perfil', {
+        url: '/perfil',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/perfil.html',
+                controller: 'PerfilCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.editar', {
-    url: '/editar',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/editar.html',
-        controller: 'EditarCtrl'
-      }
-    }
-  })
+    .state('loggedin.editar', {
+        url: '/editar',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/editar.html',
+                controller: 'EditarCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.cambiar-avatar', {
-    url: '/cambiar-avatar',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/cambiar-avatar.html',
-        controller: 'CambiarAvatarCtrl'
-      }
-    }
-  })
+    .state('loggedin.cambiar-avatar', {
+        url: '/cambiar-avatar',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/cambiar-avatar.html',
+                controller: 'CambiarAvatarCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.actividad', {
-    url: '/actividad',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/actividad.html',
-        controller: 'ActividadCtrl'
-      }
-    }
-  })
+    .state('loggedin.actividad', {
+        url: '/actividad',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/actividad.html',
+                controller: 'ActividadCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.beneficios', {
-    url: '/beneficios',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/beneficios.html',
-        controller: 'BeneficiosCtrl'
-      }
-    }
-  })
+    .state('loggedin.beneficios', {
+        url: '/beneficios',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/beneficios.html',
+                controller: 'BeneficiosCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.changuitos', {
-    url: '/changuitos',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/changuitos.html',
-        controller: 'ChanguitosCtrl'
-      }
-    }
-  })
+    .state('loggedin.changuitos', {
+        url: '/changuitos',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/changuitos.html',
+                controller: 'ChanguitosCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.tienda', {
-    url: '/tienda',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/tienda.html',
-        controller: 'TiendaCtrl'
-      }
-    }
-  })
+    .state('loggedin.tienda', {
+        url: '/tienda',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/tienda.html',
+                controller: 'TiendaCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.musica', {
-    url: '/musica',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/musica.html',
-        controller: 'MusicaCtrl'
-      }
-    }
-  })
+    .state('loggedin.musica', {
+        url: '/musica',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/musica.html',
+                controller: 'MusicaCtrl'
+            }
+        }
+    })
 
-  .state('loggedin.blog', {
-    url: '/blog',
-    views: {
-      'loggedin': {
-        templateUrl: 'templates/blog.html',
-        controller: 'BlogCtrl'
-      }
-    }
-  })
+    .state('loggedin.blog', {
+        url: '/blog',
+        views: {
+            'loggedin': {
+                templateUrl: 'templates/blog.html',
+                controller: 'BlogCtrl'
+            }
+        }
+    })
 
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,12 @@
-angular.module('kipling.controllers', []).constant('ApiEndpoint', {
-  url: 'http://localhost:8100/api'
-})
-// For the real endpoint, we'd use this
-// .constant('ApiEndpoint', {
-//  url: 'http://cors.api.com/api'
-// })
+angular.module('kipling.controllers', [])
+
+.constant('ApiEndpoint', {
+        url: 'http://localhost:8100/api'
+    })
+    // For the real endpoint, we'd use this
+    // .constant('ApiEndpoint', {
+    //  url: 'http://cors.api.com/api'
+    // })
 
 .controller('MainCtrl', function($scope) {
 
@@ -46,123 +48,129 @@ angular.module('kipling.controllers', []).constant('ApiEndpoint', {
     $scope.register = function() {
         $scope.user.terms = $scope.terms;
 
-        $http.post('http://imaginista.mx/mobileadmin/public/user/register', $scope.user).then(function(resp){
+        $http.post('http://imaginista.mx/mobileadmin/public/user/register', $scope.user).then(function(resp) {
             if (resp.data.status == 'ok') {
                 $state.go('loggedin.perfil');
             } else {
                 alert(resp.data.status);
             }
-        }, function(resp){
+        }, function(resp) {
             alert(resp.data.status);
         });
     }
 })
 
-.controller('LoginCtrl', function($scope, $http, $state, $localstorage, ApiEndpoint) {
-    $scope.user = {};
+.controller('LoginCtrl', function($scope, $http, $state, $localstorage, ) {
+            $scope.user = {};
 
-    $scope.login = function(){
-        $http.post(ApiEndpoint.url+ '/login', {
-          "user": $scope.user.email,
-          "pass": $scope.user.password
-        }).then(function(resp){
-            if (resp.status === 200 && resp.data === true) {
-                $localstorage.setObject('user', $scope.user);
-                $state.go('loggedin.perfil');
-              } else {
-                alert('El e-mail o contraseña ingresados, no son correctos ');
-            }
-        }, function(resp){
-            alert(resp.data.status);
-        });
-    }
-})
+            $scope.login = function() {
+                $http.post(ApiEndpoint.url + '/login', {
+                    "user": $scope.user.email,
+                    "pass": $scope.user.password
+                }).then(function(resp) {
+                        if (resp.status === 200 && resp.data === true) {
+                            $localstorage.setObject('user', $scope.user);
+                            $state.go('loggedin.blog');
+                            else {
+                                alert(
+                                    'El e-mail o contraseña ingresados, no son correctos '
+                                );
+                            }
+                        },
+                        function(resp) {
+                            alert(resp.data.status);
+                        });
+                }
+            })
 
-.controller('LoggedInCtrl', function($scope, $ionicModal) {
+        .controller('LoggedInCtrl', function($scope, $ionicModal) {
 
-    $ionicModal.fromTemplateUrl('my-modal.html', {
-        scope: $scope,
-        animation: 'fade-in'
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
+            $ionicModal.fromTemplateUrl('my-modal.html', {
+                scope: $scope,
+                animation: 'fade-in'
+            }).then(function(modal) {
+                $scope.modal = modal;
+            });
 
-    $scope.openModal = function() {
-        $scope.modal.show();
-    };
+            $scope.openModal = function() {
+                $scope.modal.show();
+            };
 
-    $scope.closeModal = function() {
-        $scope.modal.hide();
-    };
+            $scope.closeModal = function() {
+                $scope.modal.hide();
+            };
 
-})
+        })
 
-.controller('PerfilCtrl', function($scope, $http, ApiEndpoint, $localstorage) {
-    $scope.user = $localstorage.getObject('user');
+        .controller('PerfilCtrl', function($scope, $http, ApiEndpoint, $localstorage) {
+            $scope.user = $localstorage.getObject('user');
 
 
-    $http.post(ApiEndpoint.url+ '/pointsByUser', {
-      "mail": $scope.user.email
-    }).then(function(resp){
-        if (resp.status === 200) {
-            $scope.user.points = resp.data;
-            
-            console.log("dami", $scope.user);
-          } else {
-            alert('Error inesperado');
-            console.log("Response", resp);
-        }
-    }, function(resp){
-        alert(resp.data.status);
-    });
-
-})
-
-.controller('EditarCtrl', function($scope, $localstorage, $http, $state) {
-
-    $scope.user = {};
-
-    $scope.user = $localstorage.getObject('user');
-
-    $scope.update = function(){
-        $http.put('http://imaginista.mx/mobileadmin/public/user/update/' + $scope.user.id, $scope.user).then(function(resp){
-            if (resp.data.status == 'ok') {
-                $localstorage.setObject('user', resp.data.user);
-                $state.go('loggedin.perfil');
-            } else {
+            $http.post(ApiEndpoint.url + '/pointsByUser', {
+                "mail": $scope.user.email
+            }).then(function(resp) {
+                if (resp.status === 200) {
+                    $scope.user.points = resp.data;
+                } else {
+                    alert('Error inesperado');
+                    console.log("Response", resp);
+                }
+            }, function(resp) {
                 alert(resp.data.status);
+            });
+
+        })
+
+        .controller('EditarCtrl', function($scope, $localstorage, $http, $state) {
+
+            $scope.user = {};
+
+            $scope.user = $localstorage.getObject('user');
+
+            $scope.update = function() {
+                $http.put(
+                    'http://imaginista.mx/mobileadmin/public/user/update/' +
+                    $scope.user.id, $scope.user).then(function(resp) {
+                    if (resp.data.status == 'ok') {
+                        $localstorage.setObject('user', resp.data
+                            .user);
+                        $state.go('loggedin.perfil');
+                    } else {
+                        alert(resp.data.status);
+                    }
+                }, function(resp) {
+                    alert(resp.data.status);
+                });
             }
-        }, function(resp){
-            alert(resp.data.status);
-        });
-    }
 
-})
+        })
 
-.controller('CambiarAvatarCtrl', function($scope) {
+        .controller('CambiarAvatarCtrl', function($scope) {
 
-})
+        })
 
-.controller('ActividadCtrl', function($scope) {
+        .controller('ActividadCtrl', function($scope) {
 
-})
+        })
 
-.controller('BeneficiosCtrl', function($scope) {
+        .controller('BeneficiosCtrl', function($scope) {
 
-})
+        })
 
-.controller('ChanguitosCtrl', function($scope) {
+        .controller('ChanguitosCtrl', function($scope) {
 
-})
+        })
 
-.controller('TiendaCtrl', function($scope) {
+        .controller('TiendaCtrl', function($scope) {
 
-})
+        })
 
-.controller('MusicaCtrl', function($scope) {
+        .controller('MusicaCtrl', function($scope) {
 
-})
+        })
 
-.controller('BlogCtrl', function($scope) {
-
-})
+        .controller('BlogCtrl', function($scope, blogService) {
+            blogService.getTopTen().then(function(topeTenItems) {
+                $scope.topTen = topeTenItems;
+            });
+        })
