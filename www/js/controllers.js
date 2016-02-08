20b601276762c23220d62e2f3acbb2ff4cc8ac82
dami <droig@live.com>
2016-02-08 19:53:14 -0300

    typos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -60,117 +60,118 @@ angular.module('kipling.controllers', [])
     }
 })
 
-.controller('LoginCtrl', function($scope, $http, $state, $localstorage, ) {
-            $scope.user = {};
+.controller('LoginCtrl', function($scope, $http, $state, $localstorage, ApiEndpoint) {
+    $scope.user = {};
 
-            $scope.login = function() {
-                $http.post(ApiEndpoint.url + '/login', {
-                    "user": $scope.user.email,
-                    "pass": $scope.user.password
-                }).then(function(resp) {
-                        if (resp.status === 200 && resp.data === true) {
-                            $localstorage.setObject('user', $scope.user);
-                            $state.go('loggedin.blog');
-                            else {
-                                alert(
-                                    'El e-mail o contraseña ingresados, no son correctos '
-                                );
-                            }
-                        },
-                        function(resp) {
-                            alert(resp.data.status);
-                        });
-                }
-            })
-
-        .controller('LoggedInCtrl', function($scope, $ionicModal) {
-
-            $ionicModal.fromTemplateUrl('my-modal.html', {
-                scope: $scope,
-                animation: 'fade-in'
-            }).then(function(modal) {
-                $scope.modal = modal;
-            });
-
-            $scope.openModal = function() {
-                $scope.modal.show();
-            };
-
-            $scope.closeModal = function() {
-                $scope.modal.hide();
-            };
-
-        })
-
-        .controller('PerfilCtrl', function($scope, $http, ApiEndpoint, $localstorage) {
-            $scope.user = $localstorage.getObject('user');
-
-
-            $http.post(ApiEndpoint.url + '/pointsByUser', {
-                "mail": $scope.user.email
-            }).then(function(resp) {
-                if (resp.status === 200) {
-                    $scope.user.points = resp.data;
+    $scope.login = function() {
+        $http.post(ApiEndpoint.url + '/login', {
+            "user": $scope.user.email,
+            "pass": $scope.user.password
+        }).then(function(resp) {
+                if (resp.status === 200 && resp.data === true) {
+                    $localstorage.setObject('user', $scope.user);
+                    $state.go('loggedin.blog');
                 } else {
-                    alert('Error inesperado');
-                    console.log("Response", resp);
+                    alert(
+                        'El e-mail o contraseña ingresados, no son correctos '
+                    );
                 }
-            }, function(resp) {
+            },
+            function(resp) {
                 alert(resp.data.status);
             });
+    }
+})
 
-        })
+.controller('LoggedInCtrl', function($scope, $ionicModal) {
 
-        .controller('EditarCtrl', function($scope, $localstorage, $http, $state) {
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'fade-in'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-            $scope.user = {};
+    $scope.openModal = function() {
+        $scope.modal.show();
+    };
 
-            $scope.user = $localstorage.getObject('user');
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
 
-            $scope.update = function() {
-                $http.put(
-                    'http://imaginista.mx/mobileadmin/public/user/update/' +
-                    $scope.user.id, $scope.user).then(function(resp) {
-                    if (resp.data.status == 'ok') {
-                        $localstorage.setObject('user', resp.data
-                            .user);
-                        $state.go('loggedin.perfil');
-                    } else {
-                        alert(resp.data.status);
-                    }
-                }, function(resp) {
-                    alert(resp.data.status);
-                });
+})
+
+.controller('PerfilCtrl', function($scope, $http, ApiEndpoint,
+    $localstorage) {
+    $scope.user = $localstorage.getObject('user');
+
+
+    $http.post(ApiEndpoint.url + '/pointsByUser', {
+        "mail": $scope.user.email
+    }).then(function(resp) {
+        if (resp.status === 200) {
+            $scope.user.points = resp.data;
+        } else {
+            alert('Error inesperado');
+            console.log("Response", resp);
+        }
+    }, function(resp) {
+        alert(resp.data.status);
+    });
+
+})
+
+.controller('EditarCtrl', function($scope, $localstorage, $http, $state) {
+
+    $scope.user = {};
+
+    $scope.user = $localstorage.getObject('user');
+
+    $scope.update = function() {
+        $http.put(
+            'http://imaginista.mx/mobileadmin/public/user/update/' +
+            $scope.user.id, $scope.user).then(function(resp) {
+            if (resp.data.status == 'ok') {
+                $localstorage.setObject('user', resp.data
+                    .user);
+                $state.go('loggedin.perfil');
+            } else {
+                alert(resp.data.status);
             }
+        }, function(resp) {
+            alert(resp.data.status);
+        });
+    }
 
-        })
+})
 
-        .controller('CambiarAvatarCtrl', function($scope) {
+.controller('CambiarAvatarCtrl', function($scope) {
 
-        })
+})
 
-        .controller('ActividadCtrl', function($scope) {
+.controller('ActividadCtrl', function($scope) {
 
-        })
+})
 
-        .controller('BeneficiosCtrl', function($scope) {
+.controller('BeneficiosCtrl', function($scope) {
 
-        })
+})
 
-        .controller('ChanguitosCtrl', function($scope) {
+.controller('ChanguitosCtrl', function($scope) {
 
-        })
+})
 
-        .controller('TiendaCtrl', function($scope) {
+.controller('TiendaCtrl', function($scope) {
 
-        })
+})
 
-        .controller('MusicaCtrl', function($scope) {
+.controller('MusicaCtrl', function($scope) {
 
-        })
+})
 
-        .controller('BlogCtrl', function($scope, blogService) {
-            blogService.getTopTen().then(function(topeTenItems) {
-                $scope.topTen = topeTenItems;
-            });
-        })
+.controller('BlogCtrl', function($scope, blogService) {
+    blogService.getTopTen().then(function(topeTenItems) {
+        $scope.topTen = topeTenItems;
+    });
+})
